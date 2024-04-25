c67f520c9e681e774941f4f835d5b20fb8c44300
Dipanjan <dipanjan_pal@softprodigy.com>
2024-04-25 12:13:10 +0530

    Added table view for chatting, added date cell and bottom bar for sending messages

---
+++ Let's Chat/ReceiverMessageCell.swift
@@ -0,0 +1,23 @@
+//
+//  ReceiverMessageCell.swift
+//  Let's Chat
+//
+//  Created by Sharandeep Singh on 14/04/24.
+//
+
+import UIKit
+
+class ReceiverMessageCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
