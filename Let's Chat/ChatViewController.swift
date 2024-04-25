0ad6961e8d8dca94d23a3314133ef72d9c2c324e
Dipanjan <dipanjan_pal@softprodigy.com>
2024-04-25 12:19:13 +0530

    Added some changes to make project work initially

--- Let's Chat/ChatViewController.swift
+++ Let's Chat/ChatViewController.swift
@@ -9,21 +9,23 @@ import UIKit
 
 class ChatViewController: UIViewController {
 
+    @IBOutlet weak var chatTableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+    }
+}
 
-        // Do any additional setup after loading the view.
+
+extension ChatViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = chatTableView.dequeueReusableCell(withIdentifier: "dateCell", for: indexPath)
+        return cell
     }
-    */
-
 }
