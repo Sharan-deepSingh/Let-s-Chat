4f54d7a24855a2d29e21ee24df918c24e520d071
Dipanjan <dipanjan_pal@softprodigy.com>
2024-04-27 14:33:08 +0530

    Implemented UI for sender message

--- Let's Chat/ChatViewController.swift
+++ Let's Chat/ChatViewController.swift
@@ -21,11 +21,11 @@ class ChatViewController: UIViewController {
 extension ChatViewController: UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 5
+        return 10
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = chatTableView.dequeueReusableCell(withIdentifier: "receiverMessage", for: indexPath)
+        let cell = chatTableView.dequeueReusableCell(withIdentifier: indexPath.row % 2 == 0 ? "receiverMessage": "senderMessage", for: indexPath)
         return cell
     }
 }
